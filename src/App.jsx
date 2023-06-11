5be7df80a8b8c10206aff0d6a913314494311eb2
ludavir <david_arq.violin@yahoo.com>
2023-06-11 15:17:22 -0400

    yea?

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
@@ -7,10 +6,7 @@ import './App.css';
 import Form from 'components/ContactAdd'
 import Contacts from 'components/Contacts';
 import Filter from 'components/Filter';
-import { nanoid } from 'nanoid';
 import Tittle from 'components/Tittle';
-import Notiflix from 'notiflix';
-
 
 const App = () => {
 
